c7a5e8202d0d48ce4d04e994a2564ff6f001ea2e
Nedavine <nedavine@Nedapro.local>
2016-07-21 01:32:53 +0900

    ex5

---
+++ ex5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* This is Exercise 5 */
+int main(int arg, char*argv[])
+{
+	int distance = 100;
+	
+	// this is also a comment
+	printf(" You are %d miles away.\n", distance);
+	
+	return 0;
+}
